cd0bb4643e556e78610d042823b25a00f59687ab
js <1062814672@qq.com>
2019-10-25 12:24:42 +0800

    修改了文件

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
         System.out.println("哈哈");
         System.out.println("Hello World!");
     }
-
+    public void show(){
+        for (int i=0; i<10;i++){
+            System.out.println("***");
+        }
+    }
 }
